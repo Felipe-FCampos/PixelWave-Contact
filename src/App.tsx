4b8878508158077ef2d8eded89a8444c8767a411
Felipe-FCampos <felipe.fcampos7@gmail.com>
2023-01-29 19:21:28 -0300

    atualizado 100% e consertado

--- src/App.tsx
+++ src/App.tsx
@@ -36,76 +36,72 @@ const App = () => {
         </div>
 
         <div className="title-data">
-          <h3>Nome</h3>
-          <div className="title-data-E-N">
+            <h3>Nome</h3>
             <h4>Email</h4>
             <h5>Telefone</h5>
-          </div>
         </div>
 
         <section>
           <div className="data">
-
-            <div className="data-1">
+            
               <h3>Antenor Lorenzutti</h3>
               <h4>antenorl@gmail.com</h4>
               <h5>91280-6161</h5>
-            </div>
+          </div>
+          
+          <div className="data">
 
-            <div className="data-2">
               <h3>Bruno Botelho</h3>
               <h4>brunob@mail.com</h4>
               <h5>91270-5252</h5>
-            </div>
+          </div>
 
-            <div className="data-3">
+          <div className="data">  
               <h3>Carlos Eduardo</h3>
               <h4>carlosc@gmail.com</h4>
               <h5>91260-4463</h5>
-            </div>
+          </div>
 
-            <div className="data-4">
+          <div className="data">  
               <h3>Eduardo Costa</h3>
               <h4>eduardoc@mail.com</h4>
               <h5>91250-2164</h5>
-            </div>
+          </div>
 
-            <div className="data-5">
+          <div className="data">  
               <h3>Fred Mercury</h3>
               <h4>fredm@gmail.com</h4>
               <h5>91240-1165</h5>
-            </div>
+          </div>
 
-            <div className="data-6">
+          <div className="data">  
               <h3>Giovana Moura</h3>
               <h4>giovanam@mail.com</h4>
               <h5>91230-1165</h5>
-            </div>
+          </div>
 
-            <div className="data-7">
+          <div className="data">  
               <h3>Ivan Rosolen</h3>
               <h4>ivanr@gmail.com</h4>
               <h5>91220-8167</h5>
-            </div>
-
-            <div className="data-8">
+          </div>
+            
+          <div className="data">  
               <h3>Lilian Batista</h3>
               <h4>lilianb@mail.com</h4>
               <h5>91211-6138</h5>
-            </div>
+          </div>
 
-            <div className="data-9">
+          <div className="data">
               <h3>Manoel Rezende</h3>
               <h4>manoelr@mail.com</h4>
               <h5>91222-6469</h5>
-            </div>
+          </div>
 
-            <div className="data-10">
+          <div className="data">
               <h3>Pedro Henrique</h3>
               <h4>pedrohb@mail.com</h4>
               <h5>91230-5160</h5>
-            </div>
-
           </div>
         </section>
       </body>
